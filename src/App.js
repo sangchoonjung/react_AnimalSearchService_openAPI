946be5ebbe261618435eaa3ed6d8c2fe330674cb
sangchoonjung <109258408+sangchoonjung@users.noreply.github.com>
2022-12-17 20:48:11 +0900

    22-12-17

--- src/App.js
+++ src/App.js
@@ -1,70 +1,67 @@
-import Loading from './component/etc/loadging';
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Detail from './component/detail/detail';
-import List from './component/list/list';
-import Search from './component/search/search';
-
+import Loading from "./component/etc/loadging";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Detail from "./component/detail/detail";
+import List from "./component/list/list";
+import Search from "./component/search/search";
 
 function App() {
   document.title = "유기동물 조회서비스";
   const [pets, setPets] = useState([]);
   const [selected, setSelected] = useState();
   const [loading, setLoading] = useState(true);
-
+  // console.log(selected);
   useEffect(() => {
     setLoading(true);
     const key = process.env.REACT_APP_API_KEY;
 
-    fetch(`https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?serviceKey=${key}&_type=json&numOfRows=30`)
-      .then(response => response.json())  // 외울것
+    fetch(
+      `https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?serviceKey=${key}&_type=json&numOfRows=30`
+    )
+      .then((response) => response.json())
       .then((json) => {
         setPets(json.response.body.items.item);
-        setLoading(false)
+        setLoading(false);
       })
       .catch((e) => {
-        console.log(e)
+        console.log(e);
       });
-
   }, []);
-
 
   const handleSearch = (bgnde, endde, upr_cd = "6290000") => {
     const key = process.env.REACT_APP_API_KEY;
 
-    fetch(`https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?serviceKey=${key}&_type=json&numOfRows=32&bgnde=${bgnde}&endde=${endde}&upr_cd=${upr_cd}`)
-      .then(response => response.json())  // 외울것
+    fetch(
+      `https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?serviceKey=${key}&_type=json&numOfRows=32&bgnde=${bgnde}&endde=${endde}&upr_cd=${upr_cd}`
+    )
+      .then((response) => response.json()) // 외울것
       .then((json) => {
-        setPets(json.response.body.items.item)
+        setPets(json.response.body.items.item);
         setSelected(null);
       })
       .catch((e) => {
-        console.log(e)
-      })
-  }
-
-
+        console.log(e);
+      });
+  };
 
   const handleSelected = (data) => {
     setSelected(data);
-
-  }
+  };
   const unSelected = (data) => {
     setSelected(null);
-
-  }
-
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       {loading ? <Loading /> : null}
+      <div style={{ fontSize: "30px" }}>🧡유기동물 조회서비스🧡</div>
       <Search onSearch={handleSearch} onBack={unSelected} />
-      <div className='app'>
+      <div className="app">
         {selected ? <Detail target={selected} /> : <></>}
         <List pets={pets} onSelected={handleSelected} />
       </div>
     </div>
   );
-};
+}
 
 export default App;
--- src/component/item/item.jsx
+++ src/component/item/item.jsx
@@ -1,17 +1,18 @@
-import "./item.css"
-function Item({ data,onSelected }) {
-    return (
-        <div className="item" onClick={() => onSelected(data)}>
-            <img src={data.popfile} alt={"thumnail"} className="thumnail"/>
-            <div className="inform">
-            <p className="petsNameInList">
-                {data.kindCd} ({data.colorCd})
-            </p>
-            <p>
-               {data.orgNm} {data.happenPlace}
-            </p>
-            </div>
-    </div>);
+import "./item.css";
+function Item({ data, onSelected }) {
+  return (
+    <div className="item" onClick={() => onSelected(data)}>
+      <img src={data.popfile} alt={"thumnail"} className="thumnail" />
+      <div className="inform">
+        <p className="petsNameInList">
+          {data.kindCd} ({data.colorCd})
+        </p>
+        <p>
+          {data.orgNm} {data.happenPlace}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Item;
